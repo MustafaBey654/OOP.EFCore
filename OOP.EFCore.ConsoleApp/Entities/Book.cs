2610b437b16575f1269eb13d13dbda8fa85518d2
mustafayilmaz <mustafa.er2375@gmail.com>
2023-08-12 21:34:57 +0300

    Entity Property Constraints

---
+++ OOP.EFCore.ConsoleApp/Entities/Book.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.EFCore.ConsoleApp.Entities
+{
+    public class Book
+    {
+        [Key] //Primery Key
+        public int BookId { get; set; }
+        [Required] // Zorunlu Alan
+        [MaxLength(50)]
+        public string  Title { get; set; }
+
+    }
+}
--- OOP.EFCore.ConsoleApp/Program.cs
+++ OOP.EFCore.ConsoleApp/Program.cs
@@ -6,7 +6,9 @@ namespace OOP.EFCore.ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+
+            Console.ReadKey();
         }
     }
 }
